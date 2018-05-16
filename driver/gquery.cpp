8b230fe2bef5d946c4253c181e6b4f19cd213bbb
hzchen <chzyaobaiwei@gmail.com>
2018-05-16 16:58:53 +0800

    upload a fake dataset for data loader

--- driver/gquery.cpp
+++ driver/gquery.cpp
@@ -22,13 +22,18 @@ int main(int argc, char* argv[])
 	string host_fname = std::string(argv[2]);
 
 	NaiveIdMapper * id_mapper = new NaiveIdMapper(config);
+
+	//set the in-memory layout for buffer
 	Buffer<int> * buf = new Buffer<int>(&config, id_mapper);
 	buf->InitBuf();
 	buf->SetStorage();
 	buf->SetBuf();
 
+	//init the rdma mailbox
 	RdmaMailbox<int> * mailbox = RdmaMailbox<int>(&config, id_mapper, buf);
 	mailbox->Init(host_fname);
+
+	//load the data from HDFS
 
 	worker_finalize();
 	return 0;
