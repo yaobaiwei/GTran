c916e40f50266e7cdd94641120e08ec5580d831e
Changji <aaronchangji@gmail.com>
2018-07-30 21:11:27 +0800

    Complete where functinality to support aggregate

--- actor/where_actor.hpp
+++ actor/where_actor.hpp
@@ -19,6 +19,8 @@
 #include "storage/data_store.hpp"
 #include "utils/tool.hpp"
 
+// TODO : Between in sequential
+
 class WhereActor : public AbstractActor {
 public:
 	WhereActor(int id, DataStore * data_store, int num_thread, AbstractMailbox * mailbox) : AbstractActor(id, data_store), num_thread_(num_thread), mailbox_(mailbox), type_(ACTOR_T::WHERE) {}
@@ -49,16 +51,41 @@ public:
 
 			// Create pred chain
 			vector<int> his_labels;
+			// For aggregate stored data
+			map<value_t, int> agg_datas;
+
 			his_labels.push_back(Tool::value_t2int(actor_obj.params.at(pos)));
 			Predicate_T pred_type = (Predicate_T) Tool::value_t2int(actor_obj.params.at(pos + 1));
 
 			vector<value_t> pred_params;
 			Tool::value_t2vec(actor_obj.params.at(pos + 2), pred_params);
 
-			for (auto & params : pred_params) {
-				his_labels.push_back(Tool::value_t2int(params));
+			if (pred_type == Predicate_T::WITHIN || pred_type == Predicate_T::WITHOUT) {
+				for (auto & param : pred_params) {
+					vector<value_t> tmp_agg_data;
+					int his_label = Tool::value_t2int(param);
+
+					if(!HasAggregateData(agg_t(m.qid, his_label), tmp_agg_data)) {
+						msg.meta.recver_tid = msg.meta.parent_tid;
+						mailbox_->Send(tid, msg);
+						return;
+					}
+
+					for (auto & sgl_data : tmp_agg_data) {
+						cout << "agg_data: " << Tool::DebugString(sgl_data) << endl;
+						if (agg_datas.find(sgl_data) == agg_datas.end()) {
+							agg_datas.insert(pair<value_t, int>(sgl_data, 1));
+						}
+					}
+				}
+
+				EvaluateForAggregate(msg.data, agg_datas, Tool::value_t2int(actor_obj.params.at(pos)), pred_type);
+				continue;
 			}
 
+			for (auto & param : pred_params) {
+				his_labels.push_back(Tool::value_t2int(param));
+			}
 			pred_chain.push_back(PredicateHistory(pred_type, his_labels));
 		}
 
@@ -95,30 +122,28 @@ private:
 			Predicate_T pred_type = pred.pred_type;
 			vector<int> step_labels = pred.history_step_labels;
 
-			if (step_labels.size() > 2) {
-				cout << "Not Support now, wait for aggregate." << endl;
-				continue;
-			}
+			// TODO : Between and outside
 
 			if (step_labels.at(0) == -1) { // only one history key
 				// Find the value of history_label
 
 				for (auto & data_pair : data) {
 					history_t::iterator his_itr = data_pair.first.begin();
-					value_t his_val;
-					bool isFound = false;
+					vector<value_t> his_val;
+					int counter = step_labels.size();
 
 					do {
-						if ((*his_itr).first == step_labels.at(1)) {
-							his_val = (*his_itr).second;
-							isFound = true;
-							break;
+						if ((*his_itr).first == step_labels.at(step_labels.size() - counter)) {
+							his_val.push_back((*his_itr).second);
+							counter--;
 						}
+
+						if (counter == 0) break;
 						his_itr++;
 					} while (his_itr != data_pair.first.end());
 
 					// IF not found, try next predicate
-					if (!isFound) {
+					if (counter != 0) {
 						// Clear value of this history;
 						data_pair.second.clear();
 						continue;
@@ -137,37 +162,112 @@ private:
 				}
 			} else {
 				for (auto & data_pair : data) {
-					// Find the value of two history_label and compare each other
-					// if not success, erase whole history
-					history_t::iterator his_itr = data_pair.first.begin();
+					cout << "Step label size : " << step_labels.size() << endl;
 
-					value_t val_first;
-					value_t val_second;
-					bool isFirstFound = false;
-					bool isSecondFound = false;
+					vector<value_t> his_val;
+					int counter = step_labels.size();
 
-					do {
-						if ((*his_itr).first == step_labels.at(0)) {
-							val_first = (*his_itr).second;
-							isFirstFound = true;
-						} else if ((*his_itr).first == step_labels.at(1)) {
-							val_second = (*his_itr).second;
-							isSecondFound = true;
-						}
-						his_itr++;
-					} while (his_itr != data_pair.first.end());
+					for (auto & step_label : step_labels) {
+						history_t::iterator his_itr = data_pair.first.begin();
+						do {
+							if (step_label == (*his_itr).first) {
+								his_val.push_back((*his_itr).second);
+								cout << step_label << " : " << Tool::DebugString((*his_itr).second) << endl;
+								counter--;
+								break;
+							}
+							his_itr++;
+						} while (his_itr != data_pair.first.end());
+					}
 
-					if (!isFirstFound || !isSecondFound) {
+					if (counter != 0) {
 						data_pair.second.clear();
 						continue;
 					}
 
-					if (!Evaluate(pred_type, val_first, val_second)) {
-						data_pair.second.clear();
+					if (step_labels.size() > 2) {
+						cout << "Here" << endl;
+						cout << "Size of his_val : " << his_val.size();
+						PredicateValue single_pred(pred_type, vector<value_t>(his_val.begin() + 1, his_val.end()));
+						if (!Evaluate(single_pred, &his_val.at(0))) {
+							cout << "Erase" << endl;
+							data_pair.second.clear();
+						} else {
+							cout << "Pass : " << Tool::DebugString(his_val.at(0)) << endl; 
+						}
+					} else {
+						if (!Evaluate(pred_type, his_val.at(0), his_val.at(1))) {
+							data_pair.second.clear();
+						}
 					}
+
 				}
 			}
 		}
+	}
+
+	EvaluateForAggregate(vector<pair<history_t, vector<value_t>>> & data, map<value_t, int> & agg_data, int his_label, Predicate_T pred_type) {
+		if (his_label == -1) {
+			for (auto & data_pair : data) {
+				for (auto value_itr = data_pair.second.begin(); value_itr != data_pair.second.end(); ) {
+					if (pred_type == Predicate_T::WITHIN) {
+						if (agg_data.find(*value_itr) == agg_data.end()) {
+							// erase 
+							cout << "Erase: " << Tool::DebugString(*value_itr) << endl;
+							value_itr = data_pair.second.erase(value_itr);
+						} else {
+							cout << "Pass: " << Tool::DebugString(*value_itr) << endl;
+							value_itr++;
+						}
+					} else if (pred_type == Predicate_T::WITHOUT) {
+						if (agg_data.find(*value_itr) != agg_data.end()) {
+							// erase 
+							cout << "Erase: " << Tool::DebugString(*value_itr) << endl;
+							value_itr = data_pair.second.erase(value_itr);
+						} else {
+							value_itr++;
+						}
+					} else {
+						cout << "Wrong Predicate Type" << endl;
+						return;
+					}
+				}
+			}
+		} else {
+			for (auto & data_pair : data) {
+				for (auto his_itr = data_pair.first.begin(); his_itr != data_pair.first.end(); ) {
+					if ((*his_itr).first == his_label) {
+						if (pred_type == Predicate_T::WITHIN) {
+							if (agg_data.find((*his_itr).second) == agg_data.end()) {
+								// erase 
+								data_pair.second.clear();
+							}
+						} else if (pred_type == Predicate_T::WITHOUT) {
+							if (agg_data.find((*his_itr).second) != agg_data.end()) {
+								// erase 
+								data_pair.second.clear();
+							}
+						} else {
+							cout << "Wrong Predicate Type" << endl;
+							return;
+						}
+					}
+
+					his_itr++;
+				}
+			}
+		}
+	}
+
+	bool HasAggregateData(agg_t key, vector<value_t> & agg_data) {
+		data_store_->GetAggData(key, agg_data);
+
+		cout << agg_data.size() << endl;
+
+		if (agg_data.size() == 0) {
+			return false;
+		}
+		return true;
 	}
 };
 
