2c57d98c8cad8339b8fcf05cf26637d52550e076
Entityless <chhuang@cuhk.edu.hk>
2018-11-12 15:08:39 +0800

    Small fix on comment

--- utils/config.hpp
+++ utils/config.hpp
@@ -203,7 +203,7 @@ struct Config{
 			exit(-1);
 		}
 
-		[SYSTEM]
+		// [SYSTEM]
 		val = iniparser_getint(ini, "SYSTEM:NUM_WORKER_NODES", val_not_found);
 		if(val!=val_not_found) global_num_machines=val;
 		else
