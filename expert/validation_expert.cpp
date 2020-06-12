22cbc8361626e3dfad56dd1cdeae8e4e06a7cefe
Hongzhi Chen <chzyaobaiwei@gmail.com>
2020-06-13 00:15:18 +0800

    add license as the header

--- expert/validation_expert.cpp
+++ expert/validation_expert.cpp
@@ -1,7 +1,16 @@
-/* Copyright 2019 Husky Data Lab, CUHK
-
-Authors: Created by Aaron Li (cjli@cse.cuhk.edu.hk)
-*/
+// Copyright 2019 BigGraph Team @ Husky Data Lab, CUHK
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "expert/validation_expert.hpp"
 void ValidationExpert::process(const QueryPlan & qplan, Message & msg) {
